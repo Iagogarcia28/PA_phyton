82b2c56fefeb7847c644159f24c5e5035fcf1dfa
Iagogarcia28 <iago.silva2807@gmail.com>
2025-09-22 12:40:55 -0300

    Add files via upload

---
+++ avaliação bimestral 22.09 Iago.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Coloque seu nome aqui:Iago Garcia da Silva
+
+# **Atividade Avaliativa **
+# 
+# Enunciado:
+# 
+# Crie um programa em Python para avaliar uma competição de bandas de música. O programa deve:
+# 
+# Perguntar a quantidade de bandas participantes (mínimo 3).
+# 
+# Perguntar o nome de cada banda.
+# 
+# Para cada banda, receber as notas de 5 jurados (valores de 0 a 10).
+# 
+# Calcular a média das notas de cada banda e armazenar o resultado em um dicionário, onde a chave será o nome da banda e o valor será a média das notas.
+# 
+# Ao final, mostrar:
+# 
+# A média de cada banda.
+# 
+# O nome da banda vencedora (a que tiver maior média).
+
+# In[ ]:
+
+
+
+
+
+# Enunciado:
+# 
+# Crie um programa que conte quantas vezes cada palavra aparece em uma frase.
+# 
+# Pedir ao usuário para digitar uma frase
+# 
+# Imprimir a palavra e quantas vezes ela aparece
+
+# In[ ]:
+
+
+
+
+
+# Enunciado:
+# 
+# Crie um programa em python que imprima números pares
+# 
+# Pedir o usuário para digitar uma lista com mínimo 10 números
+# 
+# Criar uma nova somente com numeros pares
+# 
+# Imprimir números pares
+# 
+# 
+
+# In[ ]:
+
+
+
+
